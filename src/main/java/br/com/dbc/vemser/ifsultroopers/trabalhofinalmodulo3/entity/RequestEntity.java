62f9330712a16b9d80da74c5014a2c465ab3ae89
NicolasFiedler <nicolasxfiedler@gmail.com>
2022-03-25 19:21:01 -0300

    update requestEntity (commit do gabriel com problema)

--- src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/entity/RequestEntity.java
+++ src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/entity/RequestEntity.java
@@ -1,18 +1,46 @@
 package br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity(name = "REQUEST")
 public class RequestEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "REQUEST_SEQ")
+    @SequenceGenerator(name= "REQUEST_SEQ", sequenceName = "REQUEST_SEQ", allocationSize = 1)
+    @Column(name = "id_request")
     private Integer idRequest;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "request_description")
     private String description;
+
+    @Column(name = "goal")
     private Double goal;
+
+    @Column(name = "reached_value")
     private Double reachedValue;
-    private Integer idCategory;
-    private Integer idUser;
+
+    @Column(name = "id_category")
+    @Enumerated(EnumType.ORDINAL)
+    private Category category;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_bank_account", referencedColumnName = "id_bank_account")
+    private BankAccountEntity bankAccountEntity;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user", referencedColumnName = "id_user")
+    private UsersEntity usersEntity;
 }
